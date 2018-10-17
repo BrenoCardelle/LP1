9b0c508387984ee478da42784ad93082560fdf7a
BrenoCardelle <43342547+BrenoCardelle@users.noreply.github.com>
2018-10-17 10:09:25 -0300

    Create ex_6.5.c

---
+++ ex_6.5/ex_6.5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void strjoin(char* d, char* o1, char* o2){
+	int tam1, tam2, tamtotal;
+	tam1 = strlen(o1);
+	tam2 = strlen(o2);
+	tamtotal = tam1+tam2;
+	d[tamtotal]= *o1 + *o2;
+}
+
+int main(){
+	char* d, o1, o2;
+	printf("Entre com a primeira string: ");
+	gets(o1);
+	printf("Entre com a segunda string: ");
+	gets(o2);
+	
+	strjoin(d, o1, o2);
+	
+	printf("Juntando as strings: %s", d);
+}
